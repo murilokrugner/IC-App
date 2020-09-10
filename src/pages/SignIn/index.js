a51a9ca93c3600a64047c66cc0bb26d0ea853749
murilokrugner <murilokrugner@hotmail.com>
2020-09-09 22:10:35 -0300

    login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {Image} from 'react-native';
+import {KeyboardAvoidingView, ScrollView} from 'react-native';
+import {Container} from './styles';
+
+import InputAuth from '../../components/InputAuth';
+
+import { Form } from '@unform/mobile';
+
+import Women from '../../assets/women.jpg';
 
 function SignIn() {
+    const formRef = useRef(null);
+    const passwordRef = useRef();
+
+    async function handleSubmit() {
+
+    }
+
     return(
-        <View>
-            <Text>SIGNIN</Text>
-        </View>
+        <KeyboardAvoidingView style={{flex: 1}}>
+            <ScrollView style={{flex: 1}}>
+                <Container>
+                    <Image source={Women} style={{width: 400, height: 500}}/>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <InputAuth name="email" icon="mail" placeholder="E-mail" returnKeyType="next" keyboardType="email-address"
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            onSubmitEditing={() => passwordRef.current.focus()}
+                        />
+                        <InputAuth ref={passwordRef} name="password" icon="lock" placeholder="Senha" 
+                            secureTextEntry returnKeyType="send" autoCorrect={false}                            
+                            autoCapitalize="none"
+                            onSubmitEditing={handleSubmit}    
+                        />
+                    </Form>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>        
     )
 }
 
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex-direction: column;
+
+    justify-content: center;
+    align-items: center;
+
+    padding: 40px;
+`;
