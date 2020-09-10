510ef3ae6d185b49e5c48a597d2d0248c89a269e
murilokrugner <murilokrugner@hotmail.com>
2020-09-09 21:06:25 -0300

    first config

---
+++ src/routes/auth.routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../../pages/SignIn';
+import SignUp from '../../pages/SignUp';
+
+const Auth = createStackNavigator();
+
+function AuthRoutes() {
+  return (
+    <NavigationContainer independent={true}>
+      <Auth.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#fff' },
+        }}
+        initialRouteName="SignIn">
+        <Auth.Screen name="SignIn" component={SignIn} />
+        <Auth.Screen name="SignUp" component={SignUp} />
+      </Auth.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AuthRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AuthRoutes from './auth.routes';
+
+function Routes() {
+  return (
+    <AuthRoutes />
+  )
+}
+
+export default Routes;
