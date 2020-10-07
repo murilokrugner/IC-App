04ba0899212d7732f6f4a9da5070750ff5be6d2e
murilokrugner <murilokrugner@hotmail.com>
2020-10-07 16:42:58 -0300

    categorias

--- src/pages/Provider/Store/SelectCategory/index.js
+++ src/pages/Provider/Store/SelectCategory/index.js
@@ -1,12 +1,131 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { ActivityIndicator } from 'react-native';
 
-import { Container } from './styles';
+import {Picker} from '@react-native-community/picker';
+import { useAuth } from '../../../../hooks/auth';
+import { Container, BoxLoading, BoxPicker, BoxCategories, 
+    BoxAlignServices, BoxSelectService, TextCategory, Service, BoxDelService, DelService, BoxButtonNext, ButtonNext } from './styles';
+
+import api from '../../../../services/api';
+
+import Delete from '../../../../assets/delete.png';
 
 const SelectCategory = () => {
+  const [selectCategory, setSelectCategory] = useState('        Selecione uma categoria');
+
+  const { dataAuth } = useAuth();
+
+  const [loading, setLoading] = useState(false);
+  const [loadingCategory, setLoadingCategory] = useState(true);
+
+  const [category, setCategory] = useState({});  
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function loadCategories() {
+      const response = await api.get('productcategory');
+
+      setCategory(response.data);
+
+      setLoadingCategory(false);
+    }
+
+    async function loadCategory() {
+      setLoading(true);
+      const response = await api.get(`products?provider=${dataAuth.id}`);
+
+      setData(response.data);
+
+      setLoading(false);
+      setSelectCategory ('Selecione uma categoria');
+    }
+
+    loadCategories();
+    loadCategory();
+  }, []);
+
+  useEffect(() => {    
+    if (selectCategory !== '        Selecione uma categoria') {
+      async function saveCategory() {
+        const response = await api.post('addcategoryproduct', {
+          id_provider: dataAuth.id,
+          category: selectCategory,
+        });
+      }
+
+      async function loadCategory() {
+        setLoading(true);
+        const response = await api.get(`products?provider=${dataAuth.id}`);
+
+        setData(response.data);
+
+        setLoading(false);
+        setSelectCategory ('Selecione uma categoria');
+      }
+
+      saveCategory();
+      loadCategory();
+    }
+  }, [selectCategory]);
+
+  async function handleDelete() {
+
+  }
+
   return (
       <Container>
-          <Text>Categoria</Text>
+          <BoxPicker>
+            {loadingCategory ? (
+              <BoxLoading>
+                <ActivityIndicator color="#000" size="small" />
+              </BoxLoading>              
+            ) : (
+              <>
+                {data === undefined && (
+                <Picker
+                selectedValue={selectCategory}
+                style={{height: 20, width: 300, color: '#666360'}}
+                onValueChange={(itemValue, itemIndex) =>
+                  setSelectCategory(itemValue)
+                }>
+                  <Picker.Item key={"        Selecione uma categoria"} label={"        Selecione uma categoria"} value={"        Selecione uma categoria"}/>                                 
+                  {category.map(item => (
+                        <Picker.Item key={item.description} label={item.description} value={item.description}/>  
+                  ))}
+                </Picker> 
+                )}  
+              </>            
+            )}            
+          </BoxPicker>
+          <BoxCategories>
+            {loading ? (
+              <BoxLoading>
+                <ActivityIndicator color="#000" size="small" />
+              </BoxLoading>
+            ) : (
+              <>
+                {data !== undefined && (
+                  <>
+                    <TextCategory>Categoria adicionada: </TextCategory>
+                    <BoxAlignServices>                      
+                      {data.map(item => (                          
+                          <BoxSelectService key={item.id}>                          
+                            <Service>{item.category.description}</Service>
+                            <BoxDelService onPress={() => {handleDelete(item.id)}}>
+                              <DelService source={Delete}/>
+                            </BoxDelService>                      
+                          </BoxSelectService>                 
+                      ))}
+                    </BoxAlignServices>
+                  </>
+                )}
+              </>
+            )}
+          </BoxCategories>
+          <BoxButtonNext>
+            <ButtonNext>Avançar</ButtonNext>
+          </BoxButtonNext>
       </Container>
   )
 }
--- src/pages/Provider/Store/SelectCategory/styles.js
+++ src/pages/Provider/Store/SelectCategory/styles.js
@@ -1,5 +1,83 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import Button from '../../../../components/ButtonAuth';
+import {RectButton} from 'react-native-gesture-handler';
+const widthPerf = Math.trunc(parseInt(Dimensions.get('window').width));
+//const heightPerf = Math.trunc(parseInt(Dimensions.get('window').height));
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  height: 100%;
+`;
+
+export const BoxPicker = styled.View`
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 60px;
+
+  width: 100px;
+  height: 100px;
+`;
+
+export const BoxLoading = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BoxCategories = styled.View``;
+
+export const BoxAlignServices = styled.View`
+    width: ${widthPerf};
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;    
+`;
+
+
+export const BoxSelectService = styled.View`
+    width: 175;
+    height: 45px;
+    border-radius: 50px;
+    background-color: #f08080;
+
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+    margin: 10px;
+`;
+
+export const BoxDelService = styled(RectButton)`
+    justify-content: center;
+    align-items: center;
+    width: 25px;
+    height: 25px;
+`; 
+
+export const DelService = styled.Image`
+    width: 22px;
+    height: 22px;
+`;
+
+export const Service = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+`;
+
+export const TextCategory = styled.Text`
+  font-size: 16px;
+
+  margin: 30px;
+`;
+
+export const BoxButtonNext = styled.View`
+  margin-top: 200px;
+`;
+
+export const ButtonNext = styled(Button)`
+  width: 150px;
 `;
