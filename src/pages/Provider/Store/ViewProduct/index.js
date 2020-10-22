53537b7b7c4d0fa679e255209cf169ced56d5377
murilokrugner <murilokrugner@hotmail.com>
2020-10-22 16:55:38 -0300

    product

--- src/pages/Provider/Store/ViewProduct/index.js
+++ src/pages/Provider/Store/ViewProduct/index.js
@@ -2,10 +2,14 @@
 import React, {useState, useEffect} from 'react';
 import { ActivityIndicator } from 'react-native';
 
-import { Container } from './styles';
+import { Container, BoxLoading, BoxDescription, 
+  Description, BoxPrice, Price, ForwardPrice, 
+  BoxBrand, Brand, BoxNote, TitleNote, Note, BoxEdit, ButtonEdit } from './styles';
 import { useAuth } from '../../../../hooks/auth';
 import SwipeableParallaxCarousel from 'react-native-swipeable-parallax-carousel';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
+import api from '../../../../services/api';
 
 const ViewProduct = () => {
   const navigation = useNavigation();
@@ -14,7 +18,7 @@ const ViewProduct = () => {
 
   const route = useRoute();
 
- // const idProduct = route.param.id;
+  const idProduct = route.params.id;
 
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState([]);
@@ -26,21 +30,45 @@ const ViewProduct = () => {
       setImages(response.data);
 
       setLoading(false);
-
       
     }
+
+    loadImages();
   }, []);
 
   return (
       <Container>
-        <SwipeableParallaxCarousel
-          data={images}
-          parallax={true}
-          navigation={true}
-          titleColor={"#fff"}
-          navigationColor={"#235A5C"}
-          height={300}
-        />
+        {loading ? (
+          <BoxLoading>
+            <ActivityIndicator color="#000" size="small" />
+          </BoxLoading>
+        ) : (
+          <SwipeableParallaxCarousel
+            data={images}
+            parallax={true}
+            navigation={true}
+            titleColor={"#fff"}
+            navigationColor={"#235A5C"}
+            height={300}
+          />
+        )}
+        <BoxDescription>
+          <Description>Descrição do produto aqui</Description>
+        </BoxDescription>
+        <BoxPrice>
+          <Price>R$ 50,00 a vista</Price>
+          <ForwardPrice>R$ 55,00 a prazo</ForwardPrice>
+        </BoxPrice>
+        <BoxBrand>
+          <Brand>Marca: HONDA</Brand>
+        </BoxBrand>
+        <BoxNote>
+          <TitleNote>Observações sobre o produto: </TitleNote>
+          <Note>teste de obsenfenvnr</Note>
+        </BoxNote>
+        <BoxEdit>
+          <ButtonEdit>Editar produto</ButtonEdit>
+        </BoxEdit>
       </Container>
   );
 }
--- src/pages/Provider/Store/ViewProduct/styles.js
+++ src/pages/Provider/Store/ViewProduct/styles.js
@@ -1,5 +1,79 @@
 import styled from 'styled-components/native';
+import Button from '../../../../components/ButtonAuth';
 
 export const Container = styled.View`
-  
+    flex: 2;
+`;
+
+
+export const BoxLoading = styled.View`
+    flex-direction: row;
+    justify-content: center;
+
+    margin-top: 40px;
+`;
+
+export const BoxDescription = styled.View`
+    margin: 10px;
+    height: auto;
+    width: 100%;
+`;
+
+export const Description = styled.Text`
+    font-size: 25px;
+    font-weight: bold;
+`;
+
+export const BoxPrice = styled.View`
+    margin: 10px;
+    height: auto;
+    width: 100%;
+`;
+
+export const Price = styled.Text`
+    font-size: 22px;    
+`;
+
+export const ForwardPrice = styled.Text`
+    margin-top: 10px;
+    font-size: 20px;
+`;
+
+export const BoxBrand = styled.View`
+    margin: 10px;
+    height: auto;
+    width: 100%;
+`;
+
+export const Brand = styled.Text`
+     font-size: 18px; 
+`;
+
+export const BoxNote = styled.View`
+    margin: 10px;
+    height: auto;
+    width: auto;
+
+    border: 1px;
+
+    padding: 5px;
+`;
+
+export const TitleNote = styled.Text`
+     font-size: 18px; 
+     font-weight: bold;
+`;
+
+export const Note = styled.Text`
+     font-size: 18px; 
+`;
+
+export const BoxEdit = styled.View`
+   flex-direction: row;
+   justify-content: center;
+    padding: 10px;
+`;
+
+export const ButtonEdit = styled(Button)`
+    width: 250px;
 `;
