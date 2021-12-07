3b6c6c19188d8d5f990c5bf46f41e0d8f578780d
murilokrugner <murilokrugner@hotmail.com>
2021-12-06 21:46:07 -0300

    ip

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from "reactotron-react-native";
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: "192.168.2.100" })
+  const tron = Reactotron.configure({ host: "knowledgesoftware.kinghost.net:21046" })
     .useReactNative()
     .connect();
 
